45ede221b9989ff671a29ec730be2b46a051fcd2
Livie <llivyaspinagaleano@gmail.com>
2026-05-18 08:52:04 -0400

    Delete maior_menor.py

--- maior_menor.py
+++
@@ -1,9 +0,0 @@
-p =  int(input("Primeiro número: "))
-s =  int(input("Segundo número: "))
-
-if p > s:
-    print("O primeiro número é o maior")
-elif s > p:
-    print("O segundo número é o maior")
-else:
-    print("Os dois números são iguais")
